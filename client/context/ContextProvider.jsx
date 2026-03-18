3abc2031b0725063fdb37b9954783a8e83f6c1b5
hagay kohelet <hagay2506@gmail.com>
2026-03-18 15:00:14 +0200

    changwe start enemies

--- client/context/ContextProvider.jsx
+++ client/context/ContextProvider.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 const ThemeContext = createContext()
 
 function ContextProvider({children}) {
-    const [enemyCount, setEnemyCount] = useState(0)
+    const [enemyCount, setEnemyCount] = useState(1)
   return (
     <div>
       <ThemeContext value={{enemyCount, setEnemyCount}}>
